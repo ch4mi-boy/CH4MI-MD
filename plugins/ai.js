9da75150ddfe70e0f9958ffcd9d8366fb53a3835
ch4mi-boy <chamindulakahan999@gmail.com>
2024-12-21 13:50:44 -0700

    Create ai.js

---
+++ plugins/ai.js
@@ -0,0 +1,29 @@
+/*created by ch4mi boy 🕵
+contact dev1 94703973470♻️
+contact dev2 94723737086 ♻️
+© Copy coder alert ⚠
+*/
+
+
+
+const config = require('../config')
+const {cmd , commands} = require('../command')
+const { fetchJson } = require('../lib/functions')
+
+cmd({
+    pattern: "ai",
+    alias: ["gpt","bot"], 
+    react: "🧠",
+    desc: "ai chat.",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let data = await fetchJson(`https://chatgptforprabath-md.vercel.app/api/gptv1?q=${q}`)
+return reply(`${data.data}`)
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
